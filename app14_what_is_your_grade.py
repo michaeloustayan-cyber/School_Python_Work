06b5b23a3a301c8a74410dac4cf798bda077f0e4
Michael <michael.oustayan@gmail.com>
2025-09-14 19:47:51 -0500

    Update changes

---
+++ app14_what_is_your_grade.py
@@ -0,0 +1,11 @@
+grade = int(input('what is your grade?: '))
+if grade == 9:
+    print('Freshman')
+elif grade == 10:
+    print('Sophmore')
+elif grade == 11:
+    print('Junior')
+elif grade == 12:
+    print('Senior')
+else:
+    print('TBD')
